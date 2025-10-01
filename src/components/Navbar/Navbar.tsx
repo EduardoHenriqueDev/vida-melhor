86da6ca1910bedbd910670fd59d029050ec973ce
EduardoHenriqueDev <eduhenrimacedo@gmail.com>
2025-10-01 18:23:38 -0300

    Update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,23 @@
-import Hamburger from '../Hamburger/Hamburger'
+import { IoMdMenu } from "react-icons/io";
 import './Navbar.css'
 
 interface NavbarProps {
-  displayName: string
-  onSignOut: () => void
-  onOpenMenu?: () => void
+    displayName: string
+    onSignOut: () => void
+    onOpenMenu?: () => void
 }
 
 const Navbar = ({ displayName, onOpenMenu }: NavbarProps) => {
-  return (
-    <header className="home-header">
-      <div className="home-left">
-        <span className="welcome">Bem-vindo, {displayName}</span>
-      </div>
-      <button type="button" className="menu-button" onClick={onOpenMenu} aria-label="Abrir menu">
-        <Hamburger />
-      </button>
-    </header>
-  )
+    return (
+        <header className="home-header">
+            <div className="home-left">
+                <span className="welcome">Bem-vindo, {displayName}</span>
+            </div>
+            <button type="button" className="menu-button" onClick={onOpenMenu} aria-label="Abrir menu">
+                <IoMdMenu />
+            </button>
+        </header>
+    )
 }
 
 export default Navbar
